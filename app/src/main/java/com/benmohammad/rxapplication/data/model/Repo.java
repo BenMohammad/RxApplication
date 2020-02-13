97378cfe9482c348ab7a59796f911d7da3518204
Ben Mohammad <benmohammad593@gmail.com>
2020-02-13 17:53:41 +0000

    model classes added User & Repo...

--- app/src/main/java/com/benmohammad/rxapplication/data/model/Repo.java
+++ app/src/main/java/com/benmohammad/rxapplication/data/model/Repo.java
@@ -1,6 +1,28 @@
 package com.benmohammad.rxapplication.data.model;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Repo {
+
+    public final long id;
+    public final String name;
+    public final String description;
+    public final User owner;
+
+    @SerializedName("starqazers_count")
+    public final long stars;
+
+    @SerializedName("forks_count")
+    public final long forks;
+
+    public Repo(long id, String name, String description, User owner, long stars, long forks) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.owner = owner;
+        this.stars = stars;
+        this.forks = forks;
+    }
 
 
 }
--- app/src/main/java/com/benmohammad/rxapplication/data/model/User.java
+++ app/src/main/java/com/benmohammad/rxapplication/data/model/User.java
@@ -2,5 +2,10 @@ package com.benmohammad.rxapplication.data.model;
 
 public class User {
 
-    
+    public final String login;
+
+    public User(String login) {
+        this.login = login;
+    }
+
 }
