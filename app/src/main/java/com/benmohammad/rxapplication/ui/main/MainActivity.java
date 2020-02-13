46f2d3809e16fff27f327f07fec3e9350e52d533
Ben Mohammad <benmohammad593@gmail.com>
2020-02-13 20:41:32 +0000

    MainActivity refactored...

--- app/src/main/java/com/benmohammad/rxapplication/ui/main/MainActivity.java
+++ app/src/main/java/com/benmohammad/rxapplication/ui/main/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if(savedInstanceState == null) {
-
+            getSupportFragmentManager().beginTransaction().add(R.id.screenContainer, new ListFragment()).commit();
         }
     }
 }
